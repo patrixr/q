7caf4d6176e9cd6fdaefdf2aef51727399c7474b
Patrick <1822532+patrixr@users.noreply.github.com>
2024-11-12 13:14:56 +0800

    ADD: pair structure (2)

---
+++ pair.go
@@ -0,0 +1,13 @@
+package q
+
+type Pair[K any, V any] struct {
+	Key   K
+	Value V
+}
+
+func NewPair[K any, V any](key K, value V) Pair[K, V] {
+	return Pair[K, V]{
+		Key:   key,
+		Value: value,
+	}
+}
